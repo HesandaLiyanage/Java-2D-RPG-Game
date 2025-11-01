2cf438daa6bde90060500c5e24687972f797be65
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-01 15:38:32 +0530

    sprites animations are done

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -29,14 +29,14 @@ public class Player extends Entity {
 
     public void getPlayerImage() {
         try{
-            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/boy_right_2.png"));
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_right_2.png"));
 
 
         }
@@ -47,6 +47,9 @@ public class Player extends Entity {
 
     public void update(){
 
+        if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+            spriteCounter++;
+        }
         if (keyH.upPressed) {
             direction = "up";
             y -= speed;
@@ -63,6 +66,22 @@ public class Player extends Entity {
             direction = "right";
             x += speed;
         }
+
+        //since this update method is called every frame
+        //we need to change the sprite every so often
+        //but why 10 frames?
+        //player image changes every 10 frames.
+        //and this SUCKS.
+        //Because even when the player is not moving , character is wanenava mokada eka wage
+
+        if(spriteCounter >= 10){
+            if(spriteNum == 1){
+                spriteNum = 2;
+            } else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
     }
 
     public void draw(Graphics g2){
@@ -74,16 +93,36 @@ public class Player extends Entity {
 
         switch (direction) {
             case "up":
-                image = up1;
+                if (spriteNum == 1){
+                    image = up1;
+                }
+                else if (spriteNum == 2){
+                    image = up2;
+                }
                 break;
             case "down" :
-                image = down1;
+                if (spriteNum == 1){
+                    image = down1;
+                }
+                else if (spriteNum == 2){
+                    image = down2;
+                }
                 break;
             case "left" :
-                image = left1;
+                if (spriteNum == 1){
+                    image = left1;
+                }
+                else if (spriteNum == 2){
+                    image = left2;
+                }
                 break;
             case "right":
-                image = right1;
+                if (spriteNum == 1){
+                    image = right1;
+                }
+                else if (spriteNum == 2){
+                    image = right2;
+                }
                 break;
         }
 
